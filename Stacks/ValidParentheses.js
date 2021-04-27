594f6eaeb1b6a68a5d9d9313e56d9300a97f27c0
Khairul Taher <khairultaher@outlook.com>
2021-04-27 16:10:17 +0600

    2021-04-27.1

---
+++ Stacks/ValidParentheses.js
@@ -0,0 +1,27 @@
+const string = "{()[]}"
+
+const parens = {
+    '(': ')',
+    '{' : '}',
+    '[': ']'
+}
+
+var isValid = function (s) {
+    if (s.length === 0) return true;
+
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (parens[s[i]]) {
+            stack.push(s[i]);
+        } else {
+            const leftBracket = stack.pop();
+            const correctBracket = parens[leftBracket];
+            if (s[i] !== correctBracket) return false
+        }
+    }
+
+    return stack.length === 0;
+};
+
+console.log(isValid(string));
